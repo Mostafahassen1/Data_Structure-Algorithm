954c319df0e20d6850e7418f171682d0b3ae2ec3
mostafa_Hassen <mostafahass314@gmail.com>
2023-09-13 08:18:28 +0300

    delete the old code and add new tree

--- Code/3-tree.cpp
+++ Code/3-tree.cpp
@@ -1,121 +1,310 @@
 
-#include <vector>
-#include <cassert>
-#include <algorithm>
 #include <iostream>
+#include<algorithm>
+#include<vector>
+#include <cassert>
 #include <cmath>
 #include <queue>
 #include <deque>
 #include <string>
-
+#include<deque>
 using namespace std;
+ struct TreeNode{
+     int val {} ;
+     TreeNode *left {} ;
+     TreeNode *right {} ;
+     TreeNode( int value) : val(value) {
 
-class BinaryTree
-{
+     }
+ };
+
+class BinaryTree{
+
 private:
-    int Data{};
-    BinaryTree *left{};
-    BinaryTree *right{};
 
-public:
-    BinaryTree(int Data) : Data(Data)
-    {
-    }
+public :
+  TreeNode* root =NULL ;
 
-    void add(vector<int> value, vector<char> direction)
-    {
-        assert(value.size() == direction.size());
-        BinaryTree *current = this;
+     BinaryTree(int root_val ) :  root (new TreeNode(root_val) ) { }
+
+
+
+int _max_Node(TreeNode * current  ){
+
+   if( current == nullptr)
+      return 0 ;
+
+    int max_left = _max_Node( current->left );
+
+    int max_right = _max_Node (current->right  ) ;
+
+
+  return   max( max(max_left , max_right) , current->val ) ;
+}
+void Max_Node(){
+    int res =  _max_Node(root) ;
+    cout<<" the max node in the tree is : "<<res <<"\n";
+    return ;
+}
+
+
+void _PrintInorder( TreeNode *current){
+     if( current == NULL)
+       return ;
+
+_PrintInorder(current->left) ;
+      cout<< current->val<<" " ;
+      _PrintInorder(current->right) ;
+ }
+void printInorder(){
+    _PrintInorder(root) ;
+     cout<<"\n" ;
+}
+
+int get_tree_height(TreeNode * current){
+    if(current == NULL)
+     return 0 ;
+
+     int left_height  =  get_tree_height(current->left)  ;
+     int right_height = get_tree_height(current->right) ;
+
+     return max( left_height , right_height ) + 1 ;
+}
+void print_max_height_path(){
+    int tree_height = get_tree_height(root) ;
+    cout<< "Height of binary tree: " << tree_height <<"\n" ;
+    return ;
+}
+
+
+
+void add (vector<int> value, vector<char> direction ){
+     assert(value.size() == direction.size());
+        TreeNode* current = this->root;
         for (int i = 0; i < (int)value.size(); ++i)
         {
             if (direction[i] == 'L')
             {
                 if (!current->left)
-                    current->left = new BinaryTree(value[i]);
+                    current->left = new TreeNode(value[i]);
                 else
-                    assert(value[i] == current->left->Data);
+                    assert(value[i] == current->left->val);
                 current = current->left;
             }
             else
             {
                 if (!current->right)
-                    current->right = new BinaryTree(value[i]);
+                    current->right = new TreeNode(value[i]);
                 else
-                    assert(current->right->Data == value[i]);
+                    assert(current->right->val == value[i]);
                 current = current->right;
             }
         }
-    }
-
-
-int max_node(){
-    int res = Data ;
-    if(left)
-     res = max( res , left->max_node() ) ;
-
-    if(right)
-    res = max(res , right->max_node() ) ;
-
-    return res ;
 }
 
 
-int Height_tree(){
-    int ll = 1 , r =1 ;
-    if(left){
-       ll= left->Height_tree()+1;
+void _levelOrderTraversal( TreeNode * root) {
+     while(root == NULL )
+      return ;
 
-    }
+     queue<TreeNode *> qu ;
+      qu.push(root) ;
+      while(!qu.empty() ){
 
+          int sizeQueue = qu.size() ;
 
-    if(right){
-       r = right->Height_tree()+1;
+         while(sizeQueue-- ){
+             TreeNode * cur = qu.front() ;
+             qu.pop() ;
+             cout<<cur->val<<" " ;
 
-    }
+             if(cur->left)
+              qu.push(cur->left) ;
+              if(cur->right)
+              qu.push(cur->right) ;
+         }
+         cout<<"\n" ;
+     }
 
+}
+void PrintOrderTraversal(){
+    cout<<"print order Traversal : \n" ;
+    _levelOrderTraversal(root) ;
 
-
-    return max( ll , r ) ;
+    cout<<"\n";
 }
 
-int count_node(){
-    int coun= 1 ;
+void _ZigzagOrderTraversal(TreeNode * root ){
+    if(root == NULL)
+      return ;
 
-    if(left)
-     coun+=left->count_node() ;
+    deque<TreeNode*> deq ;
+    deq.push_back(root) ;
 
-     if(right)
-     coun+=right->count_node() ;
+    int count = 0 ;
+    while( !deq.empty() ){
 
-   return coun ;
-}
 
-bool search(int x){
+         int sz = deq.size() ;
+         while(sz--){
 
-    bool test = false ;
+             if(count % 2 == 1){
+               TreeNode* cur =deq.front() ;
+               deq.pop_front() ;
+               cout<<cur->val<<" " ;
 
-    if(test == false &&left){
-        test = left->count_node() ;
+               if(cur->left )
+                deq.push_back(cur->left) ;
+
+               if(cur->right)
+               deq.push_back(cur->right) ;
+             }
+
+             else{
+                 TreeNode* cur = deq.back() ;
+                 deq.pop_back() ;
+                  cout<<cur->val<<" " ;
+
+                  if(cur->right)
+                  deq.push_front( cur->right ) ;
+
+                  if(cur->left)
+                  deq.push_front( cur->left ) ;
+
+             }
+
+         }
+         cout<<"\n" ;
     }
 
-    if(test == false && right)
-      test = right->count_node() ;
+}
+void PrintZigzagOrderTraversal(){
+    cout<<"Print zigzag order traversel : \n" ;
+    _ZigzagOrderTraversal(root) ;
+    cout<<"\n";
 
-if(test == true)
-return true ;
-
-return false ;
 }
 
 
+bool _Check_Complet(TreeNode * root ){
+      if(root == NULL)
+       return false ;
 
+     queue<TreeNode *> qu ;
+     qu.push(root);
 
+     while( !qu.front()){
+         TreeNode * cur = qu.front() ;
+
+         qu.pop();
+         qu.push( cur->left ) ;
+         qu.push( cur->right ) ;
+     }
+
+     while( !qu.empty() && qu.front() == NULL )
+           qu.pop();
+
+     return qu.empty();
+}
+void Print_Check_Complet(){
+        cout<<"The tree is completly (T / F ) : ";
+
+        if(_Check_Complet(root ) == 0)
+          cout<<"FALSE" ;
+
+         else
+         cout<<"TRUE" ;
+
+        cout<<"\n" ;
+}
+
+int check_length_diameter(TreeNode * root ,int & diameter ){
+    if(root == NULL )
+      return 0 ;
+
+int left  = check_length_diameter(root->left  ,  diameter )  ;
+int right = check_length_diameter(root->right ,diameter  )  ;
+
+diameter = max(left + right , diameter ) ;
+
+return max(left , right ) + 1 ;
+
+}
+
+void print_length_of_the_diameter(){
+    if(root == NULL)
+      return ;
+    int diameter = 1 ;
+
+    check_length_diameter(root , diameter ) ;
+    cout<<"The length of the longest path between any two nodes is : " <<diameter<<"\n" ;
+    return ;
+}
+
+string _parenthesize(TreeNode * root ){
+    if( root == NULL)
+      return "()" ;
+
+     string rept = "("+to_string(root->val ) ;
+
+     if( root->left)
+     rept += _parenthesize(root->left) ;
+     else
+     rept +="()" ;
+
+    if( root->right)
+    rept += _parenthesize(root->right) ;
+    else
+    rept +="()" ;
+
+    rept +=")" ;
+
+    return rept ;
+
+}
+void print_parenthesize(){
+     cout<<"the string of tree : " << _parenthesize(root) <<"\n";
+     return ;
+}
+
+// if you have several tree how check it has the same value
+string _parenthesize_canonical(TreeNode * root) {
+    if( ! root)
+     return "()" ;
+     string repr = "(" + to_string(root->val ) ;
+     vector<string > v ;
+
+     if(left )
+     v.push_back( _parenthesize_canonical(root->left) ) ;
+     else
+      v.push_back("()") ;
+
+      if(right)
+     v.push_back( _parenthesize_canonical(root->right) ) ;
+     else
+      v.push_back("()") ;
+
+      sort( v.begin() , v.end() ) ;
+
+      for( int i = 0 ; i < (int) v.size() ; i++)
+      repr += v[i] ;
+
+      repr +=")" ;
+
+      return repr ;
+
+}
+void  print_parenthesize_canonical(){
+
+    cout<<" the parenthesize canonical of tree : "<<_parenthesize_canonical(root)<<"\n" ;
+}
 
 };
 
+
 int main()
 {
-    BinaryTree tree(1);
+     BinaryTree tree(1);
 
     tree.add({2, 4, 7}, {'L', 'L', 'L'});
     tree.add({2, 4, 8}, {'L', 'L', 'R'});
@@ -123,21 +312,14 @@ int main()
     tree.add({3, 6, 22}, {'R', 'R', 'L'});
      tree.add({3, 6, 11}, {'R', 'R', 'R'});
 
-   int z = 0 ;
-    BinaryTree * root = &tree;
+    tree.print_max_height_path() ;
+    tree.PrintOrderTraversal() ;
+    tree.PrintZigzagOrderTraversal();
+    tree.Print_Check_Complet();
+    tree.print_length_of_the_diameter();
+    tree.print_parenthesize();
+    tree.print_parenthesize_canonical() ;
 
 
-    cout<<"\n"<<tree.max_node() ;
-
-    cout<<"\n"<<tree.Height_tree();
-
-    cout<<"\n"<<tree.count_node();
-
-    cout<<"\n"<<tree.search(6) ;
-
-
-
-
-    return 0;
+  return 0;
 }
-
